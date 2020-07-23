e80ae500038ebb7aae14520298e81e12c311c366
msuh1204 <31930969+variablesarecool@users.noreply.github.com>
2020-07-23 12:22:41 -0400

    Added connection to database

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import psycopg2
 
 from dotenv import load_dotenv
 
@@ -7,3 +8,8 @@ client = discord.Client()
 
 load_dotenv()
 discordToken = os.getenv("DISCORD_TOKEN")
+
+databaseURL = os.getenv("DATABASE_URL")
+
+connection = psycopg2.connect(databaseURL)
+cursor = connection.cursor()
