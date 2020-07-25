2d085b5a7647685a7ef430cbbd2f60526cc9798e
msuh1204 <31930969+variablesarecool@users.noreply.github.com>
2020-07-25 17:00:15 -0400

    Changed variable name and added search function

--- filerw.py
+++ filerw.py
@@ -24,18 +24,34 @@ async def retrieveAllEntries(database):
      except Exception as e:
           logging.info("Error with retrieving entries: {}".format(e))
 
-async def insertEntry(database, event):
+async def findEntries(database, searchTerms):
+     try:
+          command = sql.SQL(
+               '''
+                    SELECT {columns} 
+                    FROM {table} 
+                    WHERE {conditions}
+                    LIMIT 1
+               ''').format(
+                    columns = sql.SQL(', ').join(
+                         sql.Identifier(database, key) for key in searchTerms.keys()    
+                    ),
+                    table = sql.Identifier(database),
+                    conditions = sql.SQL(' AND ').join(
+                         sql.Composed([sql.Identifier(database, key), sql.SQL(" = "), sql.Placeholder()]) for key in searchTerms.keys()
+                    )
+               )
+          print(command.as_string(connection))
+          print(list(searchTerms.values()))
+          cursor.execute(command, list(searchTerms.values()))
+          return cursor.fetchall()
+     except Exception as e:
+          logging.info("Error with retrieving entries with matching keys: {}".format(e))
+
+async def insertEntry(database, entry):
      commands = (
           sql.SQL('''SET TIMEZONE TO {timezone};''').format(
-               timezone = sql.Identifier(event[4])),
-          sql.SQL(
-          '''
-               SELECT Name, Channel, Datetime 
-               FROM {table} 
-               WHERE Name = %s AND Channel = %s AND DATETIME = %s
-               LIMIT 1
-          ''').format(
-               table = sql.Identifier(database)),
+               timezone = sql.Identifier(entry[4])),
           sql.SQL(
           '''
                INSERT INTO {table}
@@ -49,27 +65,27 @@ async def insertEntry(database, event):
           logging.info("Error with setting time zone: {}".format(e))
      else:
           try:
-               match = cursor.execute(commands[1], (event[0], event[2], event[3]))
+               match = await findEntries("events", {"name" : entry[0], "channel" : entry[2], "datetime" : entry[3]})
                if match != None:
                     raise errors.RepetitionError
           except errors.RepetitionError:
                logging.info("Entry already exists")
                raise errors.RepetitionError
           except Exception as e:
-               logging.info("Error with retrieving matching events: {}".format(e)) 
+               logging.info("Error with retrieving matching entries: {}".format(e)) 
           else:
                try:
-                    cursor.execute(commands[2], (event[0], event[1], event[2], event[3]))
+                    cursor.execute(commands[1], (entry[0], entry[1], entry[2], entry[3]))
                     connection.commit()
                except Exception as e:
                     logging.info("Error with inserting data: {}".format(e))
 
-async def deleteEntry(database, key, event):
+async def deleteEntry(database, key, entry):
      try:
           cursor.execute(
                sql.SQL("DELETE FROM {table} WHERE {column} = %s;").format(
                     table = sql.Identifier(database),
-                    column = sql.Identifier(database, key)), [event[0]])
+                    column = sql.Identifier(database, key)), [entry[0]])
           connection.commit()
      except Exception as e:
           logging.info("Error with deleting data: {}".format(e))
